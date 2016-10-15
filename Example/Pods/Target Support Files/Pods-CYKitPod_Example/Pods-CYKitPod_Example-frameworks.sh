34f9eb367de98b3bbb756f880a046f6fdf6921ec
曹源 <jorion.apple@hotmail.com>
2016-10-15 08:46:37 +0800

    updated

--- Example/Pods/Target Support Files/Pods-CYKitPod_Example/Pods-CYKitPod_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-CYKitPod_Example/Pods-CYKitPod_Example-frameworks.sh
@@ -86,10 +86,8 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/AFNetworking/AFNetworking.framework"
   install_framework "$BUILT_PRODUCTS_DIR/CYKitPod/CYKitPod.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/YohunlUtilsPod/YohunlUtilsPod.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/AFNetworking/AFNetworking.framework"
   install_framework "$BUILT_PRODUCTS_DIR/CYKitPod/CYKitPod.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/YohunlUtilsPod/YohunlUtilsPod.framework"
 fi
